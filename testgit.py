81a6a292c6506dc39b0b59ad7dc071e4647d3a8f
lehalms <mslehal@lkc.ac.in>
2018-12-25 22:16:43 +0530

    2nd version

--- testgit.py
+++ testgit.py
@@ -1,1 +1,20 @@
-print('this only for testing')
+import random
+winning_num=random.randint(1,100)
+guess_num=int(input("guess any number between (1,100): "))
+guess=1
+game_over=False
+while not game_over:
+    if guess_num==winning_num:
+        print(f"you win in {guess} guess")
+        game_over=True
+    else:
+        if guess_num<winning_num:
+            print("too low")
+            # guess+=1
+            # guess_num=int(input("Again guess: "))
+        else:
+            print("too high")
+            # guess+=1
+            # guess_num=int(input("Again guess; "))
+        guess+=1
+        guess_num=int(input("Again guess: "))
